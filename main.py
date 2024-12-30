cd9f6331a13eccdf58e05e9cf68ab201ac966ad5
kassslll <kasslesson@gmail.com>
2024-12-30 20:05:29 +0400

    create class a Car

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+class Car():
+    def __init__(self, mark: str, model: str, year: int, *args, **kwargs) -> None:
+        self.mark = mark
+        self.model = model
+        self.year = year
+
+
+def calculate(random_value: int = 0) -> int:
+    return random_value**random_value
+
+
 def main() -> None:
     print("Hello world!")
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
