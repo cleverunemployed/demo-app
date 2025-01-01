f2ddd29ba6b3cc7da38c032b810d27b3023c1ed0
kassslll <kasslesson@gmail.com>
2025-01-01 17:03:35 +0400

    add function for to repair car

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ class Car():
         self.mark = mark
         self.model = model
         self.year = year
+        self.amount_kilometres = 10000
 
+    def to_track(self, kilometres: int) -> bool:
+        if self.amount_kilometres <= 0:
+            return False
+        
+        self.amount_kilometres -= kilometres
+
+        return True
+
+    def to_repair(self, percent_repair: int) -> None:
+        self.amount_kilometres += 10000*percent_repair//100
 
 def calculate(random_value: int = 0) -> int:
     return random_value**random_value
